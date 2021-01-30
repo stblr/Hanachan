50444f547f7a4c5390b217b2a3ebf33e2820c5de
Pablo Stebler <pablo@stebler.xyz>
2021-01-31 00:13:52 +0100

    Print usage info

--- main.c
+++ main.c
@@ -53,6 +53,7 @@ int main(int argc, char **argv) {
         int ret = 1;
 
         if (argc != 3) {
+                printf("Usage: hanachan <ghost.rkg> <dump.bin>\n");
                 return ret;
         }
 
