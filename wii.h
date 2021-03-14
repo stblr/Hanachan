8691a5308cc1e163994e892f71c523209dfa737d
Pablo Stebler <pablo@stebler.xyz>
2021-03-14 21:34:44 +0100

    Remove C code

--- wii.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "util.h"
-
-f32 wii_sqrtf(f32 val);
-
-f32 wii_sinf(f32 val);
-
-f32 wii_cosf(f32 val);
--- yaz.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "util.h"
-
-bool yaz_decompress(u32 *dst_size, u8 **dst, u32 src_size, const u8 *src);
