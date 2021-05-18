08349b6cc906b9de3361e139e7272dd292028777
Pablo Stebler <pablo@stebler.xyz>
2021-05-18 22:46:19 +0200

    Update rkrd parser to version 2

--- src/fs/rkrd.rs
+++ src/fs/rkrd.rs
@@ -19,7 +19,7 @@ impl Parse for Rkrd {
         input
             .take::<u32>()
             .filter(|fourcc| *fourcc == u32::from_be_bytes(*b"RKRD"))?;
-        input.take::<u32>().filter(|version| *version == 1)?;
+        input.take::<u32>().filter(|version| *version == 2)?;
 
         let frames = iter::from_fn(|| (!input.is_empty()).then(|| input.take()))
             .collect::<Result<_, _>>()?;
@@ -35,10 +35,10 @@ pub struct Frame {
     pub pos: Vec3,
     pub vel0: Vec3,
     pub speed1: f32,
+    pub speed1_soft_limit: f32,
     pub vel2: Vec3,
     pub vel: Vec3,
     pub rot_vec0: Vec3,
-    pub rot_vec1: Vec3,
     pub rot_vec2: Vec3,
     pub rot0: Quat,
     pub rot1: Quat,
@@ -47,16 +47,16 @@ pub struct Frame {
 impl Parse for Frame {
     fn parse(input: &mut &[u8]) -> Result<Frame, Error> {
         Ok(Frame {
+            rot_vec2: input.take()?,
+            speed1_soft_limit: input.take()?,
+            speed1: input.take()?,
             floor_nor: input.take()?,
             dir: input.take()?,
             pos: input.take()?,
             vel0: input.take()?,
-            speed1: input.take()?,
+            rot_vec0: input.take()?,
             vel2: input.take()?,
             vel: input.take()?,
-            rot_vec0: input.take()?,
-            rot_vec1: input.take()?,
-            rot_vec2: input.take()?,
             rot0: input.take()?,
             rot1: input.take()?,
         })
